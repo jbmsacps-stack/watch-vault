ba33aeed6d780492e6947d5c81e520bf6141b21e
Joshua Baskar <jbmsacps@gmail.com>
2026-04-29 16:26:42 +0000

    recent

--- server.js
+++ server.js
@@ -44,4 +44,8 @@ app.get("/api/search", async (req, res) => {
 app.use("/api/auth", authRoutes);
 app.use("/api/lists", listRoutes);
 app.use("/api/saved-titles", savedTitleRoutes);
-app.listen(5000, () => console.log("Server running on http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
